a658a0bb955e835d095ee90a3d5a463499a45538
colefoster <cole@thefostersonline.com>
2025-12-21 15:07:26 -0500

    Refactor color management; replace hardcoded colors with dynamic loading from MyAppColors

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -31,9 +31,9 @@ class DashboardPanelProvider extends PanelProvider
             ->path('')
             ->login(Login::class)
             ->favicon(asset('/images/pokeball.png'))
-            ->colors([
-                'primary' => Color::Red,
-            ])
+            ->colors(
+                MyAppColors::loadAppColors(),
+            )
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
--- app/Providers/Filament/MyAppColors.php
+++ app/Providers/Filament/MyAppColors.php
@@ -2,54 +2,77 @@
 
 namespace App\Providers\Filament;
 
+use Filament\Support\Colors\Color;
+
 class MyAppColors
 {
     public static function loadAppColors(): array
+    {
         return [
             'primary' => Color::Fuchsia,
 
             'physical' => '#eb5628',
             'Physical' => '#eb5628',
+
             'special' => '#375ab2',
             'Special' => '#375ab2',
+
             'status' => Color::Gray,
             'Status' => Color::Gray,
 
             'normal' => Color::Neutral,
             'Normal' => Color::Neutral,
+
             'grass' => '#3fa129',
             'Grass' => '#3fa129',
+
             'fire' => '#e62829',
             'Fire' => '#e62829',
+
             'water' => '#2980ef',
             'Water' => '#2980ef',
+
             'electric' => '#fac000',
             'Electric' => '#fac000',
+
             'ground' => '#915121',
             'Ground' => '#915121',
+
             'psychic' => '#ee4179',
             'Psychic' => '#ee4179',
+
             'dark' => '#624d4e',
             'Dark' => '#624d4e',
+
             'flying' => '#80b7ed',
             'Flying' => '#80b7ed',
+
             'rock' => '#afa981',
             'Rock' => '#afa981',
+
             'ghost' => '#704170',
             'Ghost' => '#704170',
+
             'fairy' => '#ef70ef',
             'Fairy' => '#ef70ef',
+
             'poison' => '#9141cb',
             'Poison' => '#9141cb',
+
             'fighting' => '#ff8000',
             'Fighting' => '#ff8000',
+
             'ice' => '#3dcef3',
             'Ice' => '#3dcef3',
+
             'bug' => '#91a119',
             'Bug' => '#91a119',
+
             'dragon' => '#5060e1',
             'Dragon' => '#5060e1',
+
             'steel' => '#60a1b8',
             'Steel' => '#60a1b8',
         ];
     }
+}
